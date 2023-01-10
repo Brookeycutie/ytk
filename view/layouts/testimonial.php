fcf9dcd5ad1e446f685ab2c0671bbdfc7c92363b
Brookeycutie <brookeyajayi55@gmail.com>
2023-01-10 17:04:11 +0100

    New env 1

---
+++ view/layouts/testimonial.php
@@ -0,0 +1,104 @@
+<div class="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
+      <div class="container">
+        <div class="text-center">
+          <h6 class="section-title bg-white text-center text-warning px-3">
+            Testimonial
+          </h6>
+          <h1 class="mb-5">Our Parents Talk!</h1>
+        </div>
+        <div class="owl-carousel testimonial-carousel position-relative">
+          <div class="testimonial-item text-center">
+            <img
+              class="border rounded-circle p-2 mx-auto mb-3"
+              src="assets/img/YTKlogo.png"
+              style="width: 80px; height: 80px"
+            />
+            <h5 class="mb-0">Mrs Aliyu</h5>
+            <p>Coding and Academic Classes</p>
+            <div class="testimonial-text bg-light text-center p-4">
+              <p class="mb-0">
+              Amira says, the classes were amazing, that Mrs. Mary is nice and when she got something wrong you would correct her.
+
+Her confidence across various disciplines increased. It was great for her to have the interaction and we could always tell she was having fun especially with tasks like quizzes and programming. She often celebrated with a dance when she achieved something. Thanks Mrs Mary keep up the great work!
+              </p>
+            </div>
+          </div>
+          <div class="testimonial-item text-center">
+            <img
+              class="border rounded-circle p-2 mx-auto mb-3"
+              src="assets/img/YTKlogo.png"
+              style="width: 80px; height: 80px"
+            />
+            <h5 class="mb-0">Mrs Oyedeji</h5>
+            <p>Virtual Reading Classes</p>
+            <div class="testimonial-text bg-light text-center p-4">
+              <p class="mb-0">
+              I would say that somehow you have been able to capture his attention by coming down to his level, he found it interesting to attend your class.
+
+You have been able to pass the message to him strategically.
+
+Two days ago I decided to test how well Timi's reading and confidence has come. I realised that he is a lot better than when he started the classes. 
+
+He can now identify and blend words better with minimal help. 
+
+Well done!
+              </p>
+            </div>
+          </div>
+          <div class="testimonial-item text-center">
+            <img
+              class="border rounded-circle p-2 mx-auto mb-3"
+              src="assets/img/YTKlogo.png"
+              style="width: 80px; height: 80px"
+            />
+            <h5 class="mb-0">Mrs Nola</h5>
+            <p>Virtual Math Classes</p>
+            <div class="testimonial-text bg-light text-center p-4">
+              <p class="mb-0">
+              I’d just like to say keep up the good work. You were able to keep the balance using different teaching techniques with both kids, to match their different personalities. It is something I’ve struggled with in teaching them personally, not that I was trained to be a teacher and I’ve been quite happy with your teaching methods and being able to keep their love and interest in Maths up. 
+
+I love the regular monthly feedback and updates that you give.
+              </p>
+            </div>
+          </div>
+
+          <div class="testimonial-item text-center">
+            <img
+              class="border rounded-circle p-2 mx-auto mb-3"
+              src="assets/img/YTKlogo.png"
+              style="width: 80px; height: 80px"
+            />
+            <h5 class="mb-0">Mrs Osinowo</h5>
+            <p>Virtual Yoruba Classes</p>
+            <div class="testimonial-text bg-light text-center p-4">
+              <p class="mb-0">
+              Thank you, they really enjoy their classes and take pride in learning Yoruba language as a part of their heritage and identity. Thank you for all you do.
+              </p>
+            </div>
+          </div>
+
+          <div class="testimonial-item text-center">
+            <img
+              class="border rounded-circle p-2 mx-auto mb-3"
+              src="assets/img/YTKlogo.png"
+              style="width: 80px; height: 80px"
+            />
+            <h5 class="mb-0">Mr Emma</h5>
+            <p>Virtual Math Classes</p>
+            <div class="testimonial-text bg-light text-center p-4">
+              <p class="mb-0">
+              I must say I have been happy with the progress she made with you in two key areas;
+
+You were able to boost her self-confidence from what it was;
+
+She has also made tangible improvements in her knowledge of the subject.
+
+You made a lot of improvement with keeping her engaged for the times in-between lesson days and that is very important
+
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+ 
